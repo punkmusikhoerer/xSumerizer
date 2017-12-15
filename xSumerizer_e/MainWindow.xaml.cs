730eadb812600b7dc09d1d350206b29a4c7c55f5
Sven Thöne <punkmusikhoerer@gmx.de>
2017-12-15 17:41:51 +0100

    Add files via upload
    inital commit basis fuktionalität

---
+++ xSumerizer_e/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace xSumerizer_e
+{
+    /// <summary>
+    /// Interaktionslogik für mAINWindow.xaml
+    /// </summary>
+    public partial class mAINWindow : Window
+    { SumTab aktTab;
+
+        public mAINWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            aktTab = new SumTab();
+            lvLIste_.ItemsSource = aktTab.liste;
+            txtInput.Text = "";
+            txtInput.Focus();
+
+
+
+
+
+        }//loaded
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            decimal dec = new decimal();
+            try
+            {
+                dec = Decimal.Parse(txtInput.Text);
+            }// try
+            catch {
+                MessageBox.Show("keine Zahl");
+
+            }//catch
+
+            aktTab.AddValue(dec);
+            txtInput.Text = "";
+
+            decimal sum = new decimal();
+            sum = aktTab.Calc();
+            lbSumme.Content = sum.ToString("c");
+
+            }// btnAdd_Click
+
+    }
+}
---
+++ xSumerizer_e/SumTab.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+
+namespace xSumerizer_e
+{
+    class SumTab
+    {
+       public  string kommentar;
+        public ObservableCollection<decimal> liste;
+        public DateTime datum;
+        
+            //---------------------------------
+            public SumTab()
+
+        { liste = new ObservableCollection<decimal>();
+            datum = DateTime.Now;
+            kommentar = "kein Kommentar";
+
+                
+        }//ctor
+        //-------------------------------------------
+            public void AddValue(decimal d)
+            {
+
+            datum = DateTime.Now;
+            liste.Add(d);
+
+            }// add Value
+        //------------------------------------------------
+
+    public decimal Calc()
+        {
+            decimal retval;
+            retval = 0.0M;
+
+            foreach (decimal d in liste)
+            { retval += d; }
+            return retval;
+        }// Calc
+
+    }//class
+}//namespace
